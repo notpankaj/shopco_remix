265d7b445a4e2374e92cf0287f24fc94e898d157
pankaj <iampankaj0409@gmail.com>
2025-04-25 15:45:40 +0530

    BASEURL change

--- app/api/index.ts
+++ app/api/index.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-export const BASE_URL = "http://localhost:8080";
+// export const BASE_URL = "http://localhost:8080";
+export const BASE_URL = "https://shop-co-8jvk.onrender.com";
 
 export const AxiosErrorHandler = (error: any) => {
   if (axios.isAxiosError(error)) {
