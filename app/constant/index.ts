60290fef9f148a4a545ba5205dce7a43b9808c2d
pankaj <iampankaj0409@gmail.com>
2025-05-18 07:17:35 +0530

    stripe callback url updated

--- app/constant/index.ts
+++ app/constant/index.ts
@@ -9,7 +9,7 @@ export const MAX_PRODUCT_PRICE = 10000;
 export const STRIPE_PK =
   "pk_test_51J9mGdSFABbG4PeFGYelq6UpmBLWVRvMjFF9jtBz6czX3QcsegBaqIXY287HeEY0KB6ZRrKzbsG0QnEsyuDxy8Md007TIg3sOr";
 
-const APP_URL = "http://localhost:5173";
+const APP_URL = window.location.href || "http://localhost:5173";
 export const STRIPE_SUCCESS_URL = `${APP_URL}/checkout/success`;
 export const STRIPE_CANCEL_URL = `${APP_URL}/checkout/cancel`;
 
